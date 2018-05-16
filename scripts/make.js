003c5a48b978a98314343fad0b7743e5e7876380
Jason Reed <jcreed@gmail.com>
2018-05-15 20:12:18 -0400

    generate from data

---
+++ scripts/make.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+const posts = [
+  {"dir": "2018-05-13", "title": "Slices of Presheaf Categories"},
+  {"dir":"2018-05-06", "title": "Everything I've Learned About Logic in the Last Decade or So, Part 4"},
+  {"dir":"2018-04-29", "title": "Everything I've Learned About Logic in the Last Decade or So, Part 3"},
+  {"dir":"2018-04-22", "title": "Everything I've Learned About Logic in the Last Decade or So, Part 2"},
+  {"dir":"2018-04-15", "title": "Everything I've Learned About Logic in the Last Decade or So, Part 1"},
+  {"dir":"2018-04-08", "title": "Wellen's Synthetic Differential Geometry"},
+  {"dir":"2018-04-01", "title": "Doing $k$-means with intermediate points"},
+];
+
+let html = `<!DOCTYPE html>
+<html>
+  <body>
+	 <ul>
+`;
+
+posts.forEach(p => html += `    <li><a href="posts/${p.dir}/index.html">${p.title}</a>\n`);
+
+html += `    </ul>
+  </body>
+</html>`;
+
+fs.writeFileSync(__dirname + '/../index.html', html, 'utf8');
+
+let rss = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+  <channel>
+	 <title>jcreedcmu.github.io</title>
+	 <description>Occasional blogging about math</description>
+	 <link>https://jcreedcmu.github.io/index.html</link>
+	 <pubDate>${gmtDate(posts[0])}</pubDate>\n`;
+
+function gmtDate(p) {
+  return new Date(p.dir).toUTCString();
+}
+
+posts.forEach(p => {
+  rss += `    <item>
+      <title>${p.title}</title>
+		<link>https://jcreedcmu.github.io/posts/${p.dir}/index.html</link>
+      <pubDate>${gmtDate(p)}</pubDate>
+    </item>\n`;
+});
+
+rss += '</rss>\n';
+
+fs.writeFileSync(__dirname + '/../rss.xml', rss, 'utf8');
