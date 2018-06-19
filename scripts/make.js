b86cc8bc3bab2a7f27eea65777bfe48477740aa8
Jason Reed <jcreed@gmail.com>
2018-06-19 12:00:20 -0400

    post

--- scripts/make.js
+++ scripts/make.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const posts = [
+  {"dir": "2018-06-17", "title": "Feeling the Yoneda Lemma in my Guts"},
   {"dir": "2018-06-10", "title": "Creative Annealing"},
   {"dir": "2018-06-03", "title": "Focusing Dissection of Exponentials In Presheaf Categories"},
   {"dir": "2018-05-27", "title": "Depending on Category Variables, Dependently"},
