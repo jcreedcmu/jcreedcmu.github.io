12602df1facb6a73824959e97120da59c532c09d
Jason Reed <jcreed@gmail.com>
2021-02-05 09:06:33 -0500

    new post

--- scripts/make.js
+++ scripts/make.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const posts = [
+  {"dir": "2021-02-04", "title": "Surface Diagrams"},
   {"dir": "2021-01-14", "title": "Trees as Fibrations"},
   {"dir": "2020-12-20", "title": "Classical Logic in Intuitionistic Logic"},
   {"dir": "2020-11-22", "title": "Directed Spaces vs. Categories"},
