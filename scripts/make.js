89ac731ee9e05ad442e07ec873d2c0c47a2177ac
Jason Reed <jcreed@gmail.com>
2021-04-03 17:39:31 -0400

    fix rss feed some more

--- scripts/make.js
+++ scripts/make.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 const posts = [
-  {"dir": "2021-04-03", "title": "Sealevel Logic", "description": '<img src="posts/2021-04-03/map.png"/>'},
+  {"dir": "2021-04-03", "title": "Sealevel Logic",
+	"description": '<img src="https://jcreedcmu.github.io/posts/2021-04-03/map.png">'},
   {"dir": "2021-03-14", "title": "Differential Geometry On Cartographic Spaces"},
   {"dir": "2021-02-04", "title": "Surface Diagrams"},
   {"dir": "2021-01-14", "title": "Trees as Fibrations"},
@@ -64,7 +65,10 @@ function gmtDate(p) {
 }
 
 posts.forEach(p => {
-  const description = p.description ?? '';
+  let description = '';
+  if ( p.description !== undefined) {
+	 description = `<![CDATA[${p.description}]]>`;
+  }
   rss += `    <item>
       <title>${p.title}</title>
 		<link>https://jcreedcmu.github.io/posts/${p.dir}/index.html</link>
